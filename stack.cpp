0573c76ce3b472397c0c1d2c2583355b1399df35
Reinhard <88302084+MrReinhard@users.noreply.github.com>
2021-08-02 11:04:16 +0700

    stack
    Tugas stack C++ semester 2

---
+++ stack.cpp
@@ -0,0 +1,122 @@
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+#define true 1
+#define false 0
+
+typedef struct node *simpul;
+struct node
+{
+  char Isi;
+  simpul next;
+};
+//======================
+//==Prototype Function==
+//======================
+void Sisip_Belakang(simpul &L, char elemen);
+void Hapus_Belakang(simpul &L);
+void Cetak(simpul L);
+
+//=================
+//==Function Main==
+//=================
+main()
+{
+  char huruf;
+  simpul L = NULL;	//Pastikan bahwa L kosong
+  cout<<"==OPERASI SINGLE LINKED LIST PADA STACK==\n\n";
+  //==================
+  //==Sisip Belakang==
+  //==================
+  cout<<endl<<endl<<"Penyisipan Stack "<<endl<<endl;
+  cout<<"Masukkan Elemen	:"; cin>>huruf;
+  Sisip_Belakang(L, huruf);
+  cout<<"Masukkan Elemen	:"; cin>>huruf;
+  Sisip_Belakang(L,huruf);
+  cout<<"Masukkan Elemen	:"; cin>>huruf;
+  Sisip_Belakang(L, huruf);
+  cout<<"Masukkan Elemen	:"; cin>>huruf;
+  Sisip_Belakang(L,huruf);
+  cout<<"Masukkan Elemen	:"; cin>>huruf;
+  Sisip_Belakang(L,huruf);
+  cout<<"Masukkan Elemen	:"; cin>>huruf;
+  Sisip_Belakang(L,huruf);
+  Cetak(L);
+  //=========================
+  //==Hapus Simpul Belakang==
+  //=========================
+  cout<<endl<<endl<<"Hapus Elemen "<<endl;
+  Hapus_Belakang(L);
+  Cetak(L);
+  cout<<endl<<endl<<"Hapus Elemen "<<endl;
+  Hapus_Belakang(L);
+  Cetak(L);
+  cout<<endl<<endl<<"Hapus Elemen "<<endl;
+  Hapus_Belakang(L);
+  Cetak(L);
+  cout<<endl<<endl<<"Hapus Elemen "<<endl;
+  Hapus_Belakang(L);
+  Cetak(L);
+  getch();
+}
+
+//*************************************
+//**FUNCTION SISIP SIMPUL DI BELAKANG**
+//*************************************
+void Sisip_Belakang(simpul &L, char elemen)
+{
+  simpul bantu, baru;
+  baru = (simpul) malloc(sizeof(simpul));
+  baru->Isi = elemen;
+  baru->next = NULL;
+  if(L == NULL)
+    L=baru;
+  else
+  {
+    bantu=L;
+    while(bantu->next != NULL)
+      bantu=bantu->next;
+    bantu->next=baru;
+  }
+}
+
+//**********************************
+//**FUNCTION HAPUS SIMPUL BELAKANG**
+//**********************************
+void Hapus_Belakang(simpul &L)
+{
+  simpul bantu, hapus;
+  if(L==NULL)
+    cout<<"Linked List Kosong .................";
+  else
+  {
+    bantu = L;
+    while(bantu->next->next != NULL)
+      bantu=bantu->next;
+    hapus = bantu->next;
+    bantu->next = NULL;
+    free(hapus);
+  }
+}
+
+//*************************************
+//**FUNCTION MENCETAK ISI LINKED LIST**
+//*************************************
+void Cetak(simpul L)
+{
+  simpul bantu;
+  if(L==NULL)
+    cout<<"Linked List Kosong ......."<<endl;
+  else
+  {
+    bantu=L;
+    cout<<endl<<"Isi Linked List	: ";
+    while (bantu->next != NULL)
+    {
+      cout<<bantu->Isi<<"->";
+      bantu=bantu->next;
+    }
+    cout<<bantu->Isi;
+  }
+}
